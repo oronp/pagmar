af79d0346d1ad9850ca3076ea008788e55c536d8
oron.pariente <oron.pariente@stargo.co>
2024-04-30 08:25:00 +0300

    Update/ added drawing to P5 JS

--- p5_script.js
+++ p5_script.js
@@ -1,4 +1,6 @@
-let emotions
+var screen_center = [0, 0];
+let emotions = {status: false};
+var points = [];
 
 function fetchEmotions() {
     fetch('http://127.0.0.1:5000/get-emotions')
@@ -11,10 +13,13 @@ function fetchEmotions() {
         .catch(console.error);
 }
 
-setInterval(fetchEmotions, 1000); // Update every second
+setInterval(fetchEmotions, 250); // Update 4 times a second
 
 function setup() {
-    createCanvas(360, 270);
+    let screen_ratio = [1920, 1080]
+    createCanvas(screen_ratio[0], screen_ratio[1]);
+    screen_center = [screen_ratio[0]/2, screen_ratio[1]/2]
+    points.push(screen_center)
 }
 
 // Draw everything
@@ -24,7 +29,7 @@ function draw() {
     fill("white")
     noStroke()
 
-    if (!emotions) return
+    if (!emotions.status) return
 
     neutral = emotions.values.neutral
     text("neutral " + neutral, 20, 20)
@@ -49,4 +54,37 @@ function draw() {
 
     dots_locations = emotions.axis_dots
     text("dots locations " + dots_locations, 20, 160)
+
+    draw_line(dots_locations)
+}
+
+function draw_line(current_dots) {
+    current_dots = [current_dots[0] + screen_center[0], current_dots[1] + screen_center[1]];
+    if (points.slice(-1)[0][0] !== current_dots[0] && points.slice(-1)[0][1] !== current_dots[1]){
+        points.push(current_dots);
+        console.log(current_dots);
+    }
+
+    stroke(255);
+    strokeWeight(1);
+    strokeJoin(ROUND)
+
+    beginShape();
+    noFill();
+
+    if (points.length > 1) {
+        curveVertex(points[0][0], points[0][1]);  // Repeat the first point for control
+    }
+
+    // Pass through all points with curveVertex
+    for (let i = 0; i < points.length; i++) {
+        curveVertex(points[i][0], points[i][1]);
+    }
+
+    // Add the last point again to act as the control point
+    if (points.length > 1) {
+        curveVertex(points[points.length - 1][0], points[points.length - 1][1]);
+    }
+
+    endShape();
 }
