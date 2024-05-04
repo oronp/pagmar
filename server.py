a182543d96b96a6c21e877b56aca010d861c93e1
maya <mayaaronson20@gmail.com>
2024-05-04 19:56:46 +0300

    Access-Control-Allow-Origin

--- server.py
+++ server.py
@@ -3,14 +3,17 @@ from flask_cors import CORS  # Import CORS
 
 from emotion_detection import Pagmar
 
-model = Pagmar(camera_number=1)
+model = Pagmar(camera_number=0)
 app = Flask(__name__)
-CORS(app)  # Enable CORS for the entire app
+# CORS(app)  # Enable CORS for the entire app
+CORS(app, resources={r"/*": {"origins": "*"}})  # Allow CORS for all origins
 
 @app.route('/get-emotions')
 def get_emotions():
     emotions = model.get_emotions()
-    return jsonify(emotions)
+    response = jsonify(emotions)
+    response.headers.add('Access-Control-Allow-Origin', '*')  # Set allowed origin
+    return response
 
 
 if __name__ == '__main__':
