9e4893f74bcd57511d5da3bc5201a30210cd1b33
maya <mayaaronson20@gmail.com>
2024-05-06 16:45:21 +0300

    Typo

--- p5_script_backup.js
+++ p5_script_backup.js
@@ -1,21 +1,18 @@
-var screen_center = [0, 0];
-let emotions = {status: false};
-var points = [];
+let emotions = { status: false };
+let points = [];
 
+let directionEase = 0.1;
+let speedEase = 0.05;
 
-directionEase = 0.1
-speedEase = 0.05
-
-
-function preload(){
-    bgImage = loadImage("files/BackgroundP5.png")
+function preload() {
+    bgImage = loadImage("files/BackgroundP5.png");
 }
 
 function fetchEmotions() {
     fetch('http://127.0.0.1:5000/get-emotions')
         .then(response => response.json())
         .then(data => {
-            emotions = data
+            emotions = data;
             // Process and display emotions here
         })
         .catch(console.error);
@@ -24,121 +21,88 @@ function fetchEmotions() {
 setInterval(fetchEmotions, 250); // Update 4 times a second
 
 function setup() {
-    let screen_ratio = [1080, 1080]
-    createCanvas(screen_ratio[0], screen_ratio[1])
-    angleMode(DEGREES)
-    screen_center = [screen_ratio[0]/2, screen_ratio[1]/2]
-    points.push(screen_center)
-    background(bgImage);
+    createCanvas(windowWidth, windowHeight);
 
-    position = createVector(width/2,height/2)
-    direction = createVector(0,1)
+    // Set the background to white
+    background(255);
 
-    splashSize = 0
+    // Load the background image
+    bgImage = loadImage("files/BackgroundP5.png", () => {
+        // Calculate the center of the window
+        let centerX = windowWidth / 2;
+        let centerY = windowHeight / 2;
+
+        // Draw the background image at the center of the canvas
+        imageMode(CENTER);
+        image(bgImage, centerX, centerY);
+
+        // Initialize other variables
+        angleMode(DEGREES);
+        points.push([centerX, centerY]);
+        position = createVector(centerX, centerY);
+        direction = createVector(0, 1);
+        splashSize = 0;
+    });
 }
 
-// Draw everything
 function draw() {
-    // translate(-width/2,-height/2)
-
-    if (!emotions.status){
-        no_face_detection()
-    }
-    else {
-        dots_locations = emotions.axis_dots
-        // draw_line(dots_locations)
-        newTest(dots_locations)
-        splashSize = 0
+    if (!emotions.status) {
+        no_face_detection();
+    } else {
+        dots_locations = emotions.axis_dots;
+        newTest(dots_locations);
+        splashSize = 0;
     }
 }
 
-function newTest(current_dots){
-    current_dots = [current_dots[0] + screen_center[0], current_dots[1] + screen_center[1]];
-    if (points.slice(-1)[0][0] !== current_dots[0] && points.slice(-1)[0][1] !== current_dots[1]){
+function newTest(current_dots) {
+    current_dots = [current_dots[0] + width / 2, current_dots[1] + height / 2];
+    if (points.slice(-1)[0][0] !== current_dots[0] && points.slice(-1)[0][1] !== current_dots[1]) {
         points.push(current_dots);
     }
 
-    nextTarget = points[points.length-1]
-    nextTarget = createVector(nextTarget[0],nextTarget[1])
+    let nextTarget = createVector(current_dots[0], current_dots[1]);
+    let lengthToTarget = position.dist(nextTarget);
+    let dirToTarget = p5.Vector.sub(nextTarget, position).normalize();
+    direction = p5.Vector.lerp(direction, dirToTarget, directionEase);
+    direction.rotate(random(-50, 50));
 
-    lengthToTarget = position.dist(nextTarget)
-    dirToTarget = p5.Vector.sub(nextTarget,position).normalize()
-    direction = p5.Vector.lerp(direction, dirToTarget, directionEase)
-    direction.rotate(random(-50,50))
-
-    // nextPosition = p5.Vector.lerp(position,nextTarget, .05)
-    moving = p5.Vector.mult(direction, lengthToTarget * speedEase)
-    nextPosition = p5.Vector.add(position, moving)
-
-    stroke(0)
-    erase()
-    point(position.x,position.y)
-    noErase()
-
-    alpha = noise(frameCount / 30) * 255
-    stroke(0)
-    ovi_kav = noise(frameCount / 30) * 2
-    strokeWeight(ovi_kav)
-
-    startLine = p5.Vector.add(position,p5.Vector.mult(direction,ovi_kav))
-
-    line(position.x,position.y,nextPosition.x,nextPosition.y)
-
-
-    position = nextPosition
-}
-
-function draw_line(current_dots) {
-    current_dots = [current_dots[0] + screen_center[0], current_dots[1] + screen_center[1]];
-    if (points.slice(-1)[0][0] !== current_dots[0] && points.slice(-1)[0][1] !== current_dots[1]){
-        points.push(current_dots);
-        console.log(current_dots);
-    }
+    let moving = p5.Vector.mult(direction, lengthToTarget * speedEase);
+    let nextPosition = p5.Vector.add(position, moving);
 
     stroke(0);
-    strokeWeight(1);
-    strokeJoin(ROUND)
+    erase();
+    point(position.x, position.y);
+    noErase();
 
-    beginShape();
-    noFill();
+    stroke(0);
+    strokeWeight(noise(frameCount / 30) * 2);
 
-    if (points.length > 1) {
-        curveVertex(points[0][0], points[0][1]);  // Repeat the first point for control
-    }
+    let startLine = p5.Vector.add(position, p5.Vector.mult(direction, noise(frameCount / 30) * 2));
 
-    // Pass through all points with curveVertex
-    for (let i = 0; i < points.length; i++) {
-        curveVertex(points[i][0], points[i][1]);
-    }
+    line(position.x, position.y, nextPosition.x, nextPosition.y);
 
-    // Add the last point again to act as the control point
-    if (points.length > 1) {
-        curveVertex(points[points.length - 1][0], points[points.length - 1][1]);
-    }
-
-    endShape();
+    position = nextPosition;
 }
 
-function no_face_detection(){
-    // This area is a spacial place where you can put the code you want to do whatever you want to do in your code later.
-    // text('no face detection', 20, 20, 20)
-    // This is the end of the function -> you can replace everything between those comments.
-    fill(0,10)
-    noStroke()
-    splashSize = splashSize + 0.1
-    // circle(position.x,position.y, splashSize)
+function no_face_detection() {
+    fill(0, 10);
+    noStroke();
+    splashSize = splashSize + 0.1;
 
-    translate(position.x,position.y)
+    translate(position.x, position.y);
+    let blobDir;
+
     beginShape();
     for (let angle = 0; angle < 360; angle += 20) {
-        blobDir = createVector(1,0).rotate(angle)
-        blobDir.mult(splashSize)
-        blobDir.mult(noise(angle / 100,frameCount / 30) * 2, )
+        blobDir = createVector(1, 0).rotate(angle);
+        blobDir.mult(splashSize);
+        blobDir.mult(noise(angle / 100, frameCount / 30) * 2);
 
-        if (angle == 0) curveVertex(blobDir.x,blobDir.y)
-        curveVertex(blobDir.x,blobDir.y)
+        if (angle == 0) curveVertex(blobDir.x, blobDir.y);
+        curveVertex(blobDir.x, blobDir.y);
     }
-    curveVertex(blobDir.x,blobDir.y)
+    curveVertex(blobDir.x, blobDir.y);
     endShape(CLOSE);
-    resetMatrix()
+    resetMatrix();
 }
