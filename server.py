224ccd4e85b817d11e06f5d26906511a6c57eb0f
maya <mayaaronson20@gmail.com>
2024-05-17 14:28:31 +0300

    Typo

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 from emotion_detection import Pagmar
 
-model = Pagmar(camera_number=0)
+model = Pagmar(camera_number=1)
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})  # Allow CORS for all origins
 
