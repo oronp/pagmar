8374f53821eafe3eab9ecee84e9b05676f1055bd
oron.pariente <oron.pariente@stargo.co>
2024-07-13 19:03:58 +0300

    Fix/ interval is faster

--- public/static/utils.js
+++ public/static/utils.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     // Capture and send an image every second
-    setInterval(captureAndSendImage, 1000);
+    setInterval(captureAndSendImage, 250);
 });
 
 // function startGetEmotions() {
